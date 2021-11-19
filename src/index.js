238eda690f5b9b13911e114ee7f82c48a4687ddf
Albert Lee <leesga8@gmail.com>
2021-11-18 20:33:19 -0800

    add functionality to show gifs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 // import anime from 'animejs/lib/anime.es.js'
+
+$(document).ready(function(){
+  $('#getGif').click(function(){
+    const search = $('#search').val();
+    $('#gif').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${search}&limit=5&offset=0&rating=g&lang=en`;
+    
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      response.data.forEach(element => $('ul#showGif').append(
+        `<li><a href='${element.url}'><img src='${element.images.downsized.url}'></a></li>`));
+      }
+  });
+});
